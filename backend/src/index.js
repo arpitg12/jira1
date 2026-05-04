1dee47fa8e306cd0f941a70c0ac8918446ef7a6e
arpitg12 <arpitagrawal1207@gmail.com>
2026-05-04 20:59:47 +0530

    UI Updates

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,7 +26,7 @@ connectDB()
 app.use(helmet());
 app.use(
   cors({
-    origin: env.corsOrigins,
+    origin: true,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
